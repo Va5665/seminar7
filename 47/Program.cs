f0fca55521ca56a7dbfe49b2e8428d1587e43a30
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-08-29 16:56:38 +0530

    добаваили

---
+++ 47/Program.cs
@@ -0,0 +1,37 @@
+﻿// 47.
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+Console.Write("Введите количество строк: ");
+var rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+var columns = Convert.ToInt32(Console.ReadLine());
+double[,] GetArray(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++) // по строчкам
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++) // по столбцам
+        {
+            matrix[i, j] = new Random().Next(-100, 100) / 10.0;
+        }
+    }
+    return matrix;
+}
+double[,] resultMatrix = GetArray(rows, columns);
+PrintArray(resultMatrix);
+
+
+void PrintArray(double[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write(inputMatrix[i, j] + "\t"); // "\t" - Tab
+        }
+        Console.WriteLine();
+    }
+}
